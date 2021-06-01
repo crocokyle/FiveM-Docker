c8ab902070921d2f2bdc7ff1477bc7fb55451fb7
crocokyle <me@kyle-dixon.com>
2021-05-31 20:16:05 -1000

    Added saving CFX Build to an environment variable

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,10 @@ RUN mkdir ~/FXServer/server-data
 RUN apt-get update \
   && apt-get install -y wget git xz-utils python3.4 python3-pip \
   && rm -rf /var/lib/apt/lists/*
-RUN python -m pip install beautifulsoup4
-RUN wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/3874-31c784e5970e835e8d1bcad6f098e7ef5a651200/fx.tar.xz 
+RUN pip3 install beautifulsoup4
+RUN pip3 install requests
+RUN wget https://raw.githubusercontent.com/crocokyle/FiveM-Docker/main/getLatestCFXBuild.py
+RUN python3 getLatestCFXBuild.py
 RUN tar xf fx.tar.xz -C ~/FXServer/server
 RUN rm fx.tar.xz
 RUN git clone https://github.com/citizenfx/cfx-server-data.git ~/FXServer/server-data
--- getLatestCFXBuild.py
+++ getLatestCFXBuild.py
@@ -1,11 +1,16 @@
 from bs4 import BeautifulSoup
 import os
+import re
 import requests
 
 URL = 'https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/'
 soup = BeautifulSoup(requests.get(URL).content)
 relative_path = soup.find_all("div", {"class": "panel-block"})[0].next_element.next_element['href']
-latest_stable = "{}{}".format(URL, relative_path[2:])
+href_text = soup.find_all("div", {"class": "panel-block"})[0].next_element.next_element.next_element
+build_number = re.findall(r'\((\d{4})\)', href_text)[0]
+latest_stable_url = "{}{}".format(URL, relative_path[2:])
 
-print(latest_stable)
-os.system("wget " + latest_stable)
+print("Latest stable build ({}) @ {}".format(build_number, latest_stable_url))
+
+os.environ["CFX_BUILD"] = build_number
+os.system("wget " + latest_stable_url)
